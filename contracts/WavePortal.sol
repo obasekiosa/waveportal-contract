670795e0b1e6d47384657183a863c4ee0befc420
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-01-17 20:10:36 +0100

    Add lottery and cool down periods

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -19,13 +19,25 @@ contract WavePortal {
     Wave[] waves;
     mapping(address => Wave[]) addressToWaves;
 
-    constructor() {
+    mapping(address => uint256) public lastWavedAt;
+
+    uint256 private seed;
+
+    constructor() payable {
         console.log("Logging from WavePortal constructor by %s", msg.sender);
-        console.log("Current max waver is %s", maxWaverAddress);
+        seed = (block.timestamp + block.difficulty) % 100;
     
     }
 
     function wave(string memory _message) public {
+
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15 minutes"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s has waved! with a message %s", msg.sender, _message);
 
@@ -40,6 +52,24 @@ contract WavePortal {
             maxWaverAddress = msg.sender;
         }
 
+        
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
+        
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+        
         emit NewWave(newWave.waver, newWave.timestamp, newWave.message);
     }
 
--- scripts/run.js
+++ scripts/run.js
@@ -1,25 +1,71 @@
 const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.1"),
+    });
     await waveContract.deployed();
 
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
+
+    // get balance
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
     console.log(waveCount.toNumber());
 
-    /**
-     * Let's send a few waves!
-     */
-    let waveTxn = await waveContract.wave("A message!");
+    // send a wave
+    let waveTxn = await waveContract.wave("This is wave #1");
     await waveTxn.wait(); // Wait for the transaction to be mined
 
-    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
-    await waveTxn.wait(); // Wait for the transaction to be mined
+
+    // chech contract balance after wave
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    waveTxn = await waveContract.wave("This is another wave #1");
+    await waveTxn.wait();
+
+    // chech randomPerson balance before second wave wave
+    contractBalance = await hre.ethers.provider.getBalance(randomPerson.address);
+    console.log(
+        "waver balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    // send another wave
+    waveTxn = await waveContract.connect(randomPerson).wave("This is wave #2");
+    otherDetails = await waveTxn.wait(); // Wait for the transaction to be mined
+    
+    console.log("Transaction details", waveTxn);
+    console.log("Other details after waiting", otherDetails);
+
+    // chech contract balance after wave
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
+
+    // chech randomPerson balance after second wave wave
+    contractBalance = await hre.ethers.provider.getBalance(randomPerson.address);
+    console.log(
+        "waver balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let allWaves = await waveContract.getAllWaves();
     console.log(allWaves);
